60b07aae1840851bc83f6d76897cd9880784ab0a
suzumikko <sandra.preysing@zoho.com>
2022-03-24 07:39:01 +0100

    solution exercise 3

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -2,10 +2,12 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 import {
-  MAIN_STORY,
-  OPINION_STORIES,
-  SECONDARY_STORIES,
+    MAIN_STORY,
+    OPINION_STORIES,
+    SECONDARY_STORIES,
 } from '../../data';
+
+import {QUERIES} from "../../constants";
 
 import SectionTitle from '../SectionTitle';
 import MainStory from '../MainStory';
@@ -14,38 +16,38 @@ import OpinionStory from '../OpinionStory';
 import Advertisement from '../Advertisement';
 
 const MainStoryGrid = () => {
-  return (
-    <Wrapper>
-      <MainStorySection>
-        <MainStory {...MAIN_STORY} />
-      </MainStorySection>
+    return (
+        <Wrapper>
+            <MainStorySection>
+                <MainStory {...MAIN_STORY} />
+            </MainStorySection>
 
-      <SecondaryStorySection>
-        <StoryList>
-          {SECONDARY_STORIES.map((story, index) => (
-            <VerticalStoryWrapper key={story.id}>
-                <SecondaryStory {...story} />
-            </VerticalStoryWrapper>
-          ))}
-        </StoryList>
-      </SecondaryStorySection>
+            <SecondaryStorySection>
+                <StoryList>
+                    {SECONDARY_STORIES.map((story, index) => (
+                        <VerticalStoryWrapper key={story.id}>
+                            <SecondaryStory {...story} />
+                        </VerticalStoryWrapper>
+                    ))}
+                </StoryList>
+            </SecondaryStorySection>
 
-      <OpinionSection>
-        <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
-          {OPINION_STORIES.map((story, index) => (
-              <VerticalStoryWrapper key={story.id}  >
-                  <OpinionStory {...story} />
-              </VerticalStoryWrapper>
-          ))}
-        </StoryList>
-      </OpinionSection>
+            <OpinionSection>
+                <SectionTitle>Opinion</SectionTitle>
+                <OpinionStoryList>
+                    {OPINION_STORIES.map((story, index) => (
+                        <OpinionStoryWrapper key={story.id}  >
+                            <OpinionStory {...story} />
+                        </OpinionStoryWrapper>
+                    ))}
+                </OpinionStoryList>
+            </OpinionSection>
 
-      <AdvertisementSection>
-        <Advertisement />
-      </AdvertisementSection>
-    </Wrapper>
-  );
+            <AdvertisementSection>
+                <Advertisement />
+            </AdvertisementSection>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
@@ -57,14 +59,55 @@ const Wrapper = styled.div`
     'advertisement';
   gap: 48px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-areas:
+      'main-story secondary-stories'
+      'advertisement advertisement'
+      'opinion-stories opinion-stories';
+    grid-template-columns: 2fr 1fr;
+    gap: 48px 0;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-areas:
+      'main-story secondary-stories opinion-stories'
+      'main-story advertisement advertisement';
+    grid-template-columns: 5fr 4fr 3fr;
+    gap: 0;
+  }
 `;
 
 const MainStorySection = styled.section`
   grid-area: main-story;
+
+  @media ${QUERIES.tabletAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
+
+    /*  @media ${QUERIES.laptopAndUp} {
+    margin-right: 16px;
+    padding-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }*/
 `;
 
 const SecondaryStorySection = styled.section`
   grid-area: secondary-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
+
+    /*  @media ${QUERIES.tabletOnly} {
+    margin-left: 16px;
+    padding-left: 16px;
+    border-left: 1px solid var(--color-gray-300);
+  }*/
 `;
 
 const VerticalStoryWrapper = styled.div`
@@ -75,17 +118,53 @@ const VerticalStoryWrapper = styled.div`
   }
 `
 
+const OpinionStoryWrapper = styled(VerticalStoryWrapper)`
+  @media ${QUERIES.tabletOnly} {
+    flex: 1;
+
+    &:not(:last-of-type) {
+      border-bottom: revert;
+      padding-bottom: revert;
+      margin-bottom: revert;
+    }
+  }
+`
+
 const StoryList = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
+const OpinionStoryList = styled(StoryList)`
+  @media ${QUERIES.tabletOnly} {
+    flex-direction: row;
+    gap: 32px;
+  }
+`
+
 const OpinionSection = styled.section`
   grid-area: opinion-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-top: -8px;
+  }
+
+    /*  @media ${QUERIES.laptopAndUp} {
+    margin-left: 16px;
+    padding-left: 16px;
+    border-left: 1px solid var(--color-gray-300);
+  }*/
 `;
 
 const AdvertisementSection = styled.section`
   grid-area: advertisement;
+
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-top: 16px;
+    padding-top: 16px;
+    border-top: 1px solid var(--color-gray-300);
+  }
 `;
 
 export default MainStoryGrid;
