c9094806ba5f48e9735ef5dacb6e1fbd6605c210
suzumikko <sandra.preysing@zoho.com>
2022-03-25 08:06:15 +0100

    solution exercise 4

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -6,6 +6,7 @@ import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
+import {QUERIES} from "../../constants";
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -36,7 +37,9 @@ const SpecialtyStoryGrid = () => {
         </SectionTitle>
         <SportsStories>
           {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
+              <SportsStoryWrapper key={data.id}>
+            <MiniStory  {...data} />
+              </SportsStoryWrapper>
           ))}
         </SportsStories>
       </SportsSection>
@@ -47,14 +50,56 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    gap: 64px;
+    grid-template-columns: minmax(0px, auto);
+    /* default: auto => minimum width = child width, 
+      set min width to 0 to allow shrink and avoid child overflowing viewport width 
+    */
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    gap: 0;
+    grid-template-columns: 1fr minmax(0px, 1fr); 
+    /* here too, because sports will else take much more space than market */
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(165px, 1fr))
+`;
 
 const SportsSection = styled.section``;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(165px, 1fr));
+  gap: 16px;
+  
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+    overflow-x: auto;
+    grid-template-columns: revert;
+    /* won't help: max-width: 100% => = minmax(0, 1fr) auf grid */
+
+  }
+`;
+
+const SportsStoryWrapper = styled.div`
+  @media ${QUERIES.tabletAndUp} {
+    min-width: 220px; /* needed to trigger overflow */
+  }
+`
 
 export default SpecialtyStoryGrid;
